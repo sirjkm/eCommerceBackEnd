eb1e21571c762d5f74a9ec731fe2c737d8349a80
Jason Manning <jasonkylemanning@gmail.com>
2021-06-10 13:25:04 -0400

    update sequelize models

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const { Sequelize } = require('sequelize');
+const mysql = require('mysql2');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
